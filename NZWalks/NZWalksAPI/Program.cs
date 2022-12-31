a8cf61d248752c72a48bcfa1fc629b9ed5b2a3bc
Marijan Novak <marijan@marijannovak.com>
2022-12-31 12:05:37 +0100

    End of Section 3

--- NZWalks/NZWalksAPI/Program.cs
+++ NZWalks/NZWalksAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalksAPI.Data;
+using NZWalksAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<NZWalksDbContext>(options => { options.UseSqlServer("name=NZWalks"); });
+
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
---
+++ NZWalks/NZWalksAPI/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+
+        public IEnumerable<Region> GetAll()
+        {
+            return nZWalksDbContext.Regions.ToList();
+        }
+    }
+}
